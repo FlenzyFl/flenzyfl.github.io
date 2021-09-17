a078952cd2f31491842ac4cbac6458cb5196e84c
FlenzyFl <87827657+FlenzyFl@users.noreply.github.com>
2021-09-17 12:01:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+		function videoPlay() {
+			video = document.getElementById('video1');
+			video.play();
+		}
+		
+		function videoPause() {
+			video = document.getElementById('video1');
+			video.pause();
+        }
